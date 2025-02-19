55958e49ad95207426fffd47f8e9be0a320daec0
Sahab Uddin Ahmad <sahabuabofficial@gmail.com>
2025-02-19 22:45:39 +0600

    mail system aaded

---
+++ mail/index.php
@@ -0,0 +1,34 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'sahabuab@gmail.com';               // SMTP username
+    $mail->Password   = 'mnqniylvfbjfdfqr';                        // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('sahabuab@gmail.com', 'Mailer');
+    $mail->addAddress('sahabuab@gmail.com', 'Joe User'); 
+
+    // Content
+    $mail->isHTML(true);                                        // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
--- src/calculator.js
+++ src/calculator.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const submitButton = document.getElementById("submit");
 
-  // altimate const
-  let toatlErro = 0;
-
-  let propertyTypeCost = 0;
-  let badroomCost = 0;
-  let interestedCost = 0;
-  let InteriorCost = 0;
+  let propertyTypeCost = {};
+  let badroomCost = {};
+  let interestedCost = {};
   let extentiosnCost = {};
+  let interiorCost = 0;
   let facilitiesCost = {};
+
+  let totalError = {};
 
   submitButton.addEventListener("click", function (event) {
     event.preventDefault();
@@ -18,22 +17,23 @@ document.addEventListener("DOMContentLoaded", function () {
     const propertyType = document.querySelector(
       'input[name="property"]:checked'
     )?.value;
-    isValidate(propertyType, "Property is required");
+    isValidate(propertyType, "propertyType", "Property is required");
+    propertyTypeCost = {};
     switch (propertyType) {
       case "Detached":
-        propertyTypeCost = 2000;
+        propertyTypeCost["Detached"] = 2000;
         break;
       case "Semi Detached":
-        propertyTypeCost = 2000;
+        propertyTypeCost["Semi Detached"] = 2000;
         break;
       case "Terrace":
-        propertyTypeCost = 2000;
+        propertyTypeCost["Terrace"] = 2000;
         break;
       case "Flat":
-        propertyTypeCost = 2000;
+        propertyTypeCost["Flat"] = 2000;
         break;
       case "Bungalow":
-        propertyTypeCost = 2000;
+        propertyTypeCost["Bungalow"] = 2000;
         break;
     }
 
@@ -41,21 +41,23 @@ document.addEventListener("DOMContentLoaded", function () {
     const projectType = document.querySelector(
       'input[name="project"]:checked'
     )?.value;
-    isValidate(projectType, "Project is required");
+    isValidate(projectType, "projectType", "Project is required");
     switch (projectType) {
       case "Interior Works":
-        InteriorCost = 2500;
+        interiorCost = 0;
+        interiorCost = 2500;
         break;
       case "Extension":
-        getExtentionsConst();
+        getExtentionsConst("Extension");
         break;
       case "Interior + Extension":
-        getExtentionsConst();
+        getExtentionsConst("Interior_Extension");
         break;
     }
 
     // get extra facilities
     const facilities = document.querySelectorAll('input[name="facilities"]');
+    facilitiesCost = {};
     facilities.forEach((facility) => {
       if (facility.checked) {
         facilitiesCost[facility.value] = 2500;
@@ -68,25 +70,26 @@ document.addEventListener("DOMContentLoaded", function () {
     const badrromIdea = document.querySelector(
       'input[name="idea"]:checked'
     )?.value;
-    isValidate(badrromIdea, "Property is required");
-    badroomCost = 2000 * parseInt(badrromIdea);
+    badroomCost = {};
+    badroomCost[badrromIdea] = 2000 * parseInt(badrromIdea);
 
     // get interested
     const interested = document.querySelector(
       'input[name="interested"]:checked'
     )?.value;
+    interestedCost = {};
     switch (interested) {
       case "Design":
-        interestedCost = 2000;
+        interestedCost["Design"] = 2000;
         break;
       case "financing":
-        interestedCost = 2000;
+        interestedCost["financing"] = 2000;
         break;
       case "findingBuilder":
-        interestedCost = 2000;
+        interestedCost["findingBuilder"] = 2000;
         break;
       case "professionals":
-        interestedCost = 2000;
+        interestedCost["professionals"] = 2000;
         break;
     }
 
@@ -94,15 +97,15 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // add extrentios data
-  function getExtentionsConst() {
-    InteriorCost = 0;
+  function getExtentionsConst(target) {
+    interiorCost = 0;
     const extentiosToggler = document.querySelectorAll(".extentiosToggler");
     extentiosToggler.forEach((ele) => {
       let card = ele.parentElement.parentElement.parentElement;
       if (ele.checked) {
         const extentionsName = card.querySelector("select").value;
         const extentionsSquar = card.querySelector("#extentionsSquar").value;
-        const extentionsInfo = card.querySelector("#extraInfo");
+        const extentionsInfo = card.querySelector("#extraInfo").value;
 
         extentiosnCost[card.children[1].getAttribute("id")] = {};
 
@@ -112,12 +115,30 @@ document.addEventListener("DOMContentLoaded", function () {
             extentionsName == "Rear Extension"
           ) {
             let name = extentionsName == "Side Extension" ? "Side" : "Rear";
-            extentiosnCost[card.children[1].getAttribute("id")][name] =
+            extentiosnCost[card.children[1].getAttribute("id")]["name"] = name;
+            extentiosnCost[card.children[1].getAttribute("id")]["price"] =
               2500 * extentionsSquar;
+            extentiosnCost[card.children[1].getAttribute("id")]["target"] =
+              target;
+            extentiosnCost[card.children[1].getAttribute("id")]["quty"] =
+              extentionsSquar;
           }
           if (extentionsName == "Side + Rear Extension") {
             extentiosnCost[card.children[1].getAttribute("id")]["both"] =
+              "both";
+            extentiosnCost[card.children[1].getAttribute("id")]["price"] =
               5000 * extentionsSquar;
+            extentiosnCost[card.children[1].getAttribute("id")]["target"] =
+              target;
+            extentiosnCost[card.children[1].getAttribute("id")]["quty"] =
+              extentionsSquar;
+          }
+          extentiosnCost[card.children[1].getAttribute("id")]["select"] =
+            card.children[1].getAttribute("id");
+          if (extentionsInfo != "") {
+            extentiosnCost[card.children[1].getAttribute("id")][
+              "extentionsInfo"
+            ] = extentionsInfo;
           }
         } else {
           isValidate(false, "Selected extention value required!");
@@ -129,9 +150,9 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   // make a validation function
-  function isValidate(argu, text) {
+  function isValidate(argu, target, text) {
     if (!argu) {
-      toatlErro += 1;
+      totalError[target] = true;
       Toastify({
         text: text,
         duration: 3000,
@@ -143,37 +164,192 @@ document.addEventListener("DOMContentLoaded", function () {
         },
       }).showToast();
       return;
+    } else {
+      delete totalError[target];
     }
   }
 
   // result maker
   function resultMaker() {
+    // result json
+    let reulstObject = {};
+
+    // total cost
     let totalCost = 0;
 
-    totalCost += propertyTypeCost ? propertyTypeCost : 0;
-    totalCost += badroomCost ? badroomCost : 0;
-    totalCost += interestedCost ? interestedCost : 0;
-    totalCost += InteriorCost ? InteriorCost : 0;
+    // make a result object
+    reulstObject["property"] = {};
+    for (const key in propertyTypeCost) {
+      if (propertyTypeCost.hasOwnProperty(key)) {
+        reulstObject["property"][key] = propertyTypeCost[key];
+      }
+    }
 
-    // extentionConst
-    for (const key in extentiosnCost) {
-      if (extentiosnCost.hasOwnProperty(key)) {
-        const element = extentiosnCost[key];
-        for (const subKey in element) {
-          if (element.hasOwnProperty(subKey)) {
-            totalCost += element[subKey] ? parseInt(element[subKey]) : 0;
+    reulstObject["badroom"] = {};
+    for (const key in badroomCost) {
+      if (key != "undefined") {
+        if (badroomCost.hasOwnProperty(key)) {
+          reulstObject["badroom"][key] = badroomCost[key];
+        }
+      }
+    }
+
+    reulstObject["interested"] = {};
+    for (const key in interestedCost) {
+      if (interestedCost.hasOwnProperty(key)) {
+        reulstObject["interested"][key] = interestedCost[key];
+      }
+    }
+
+    reulstObject["facilities"] = {};
+    for (const key in facilitiesCost) {
+      if (facilitiesCost.hasOwnProperty(key)) {
+        reulstObject["facilities"][key] = facilitiesCost[key];
+      }
+    }
+
+    reulstObject["extentions"] = {};
+    if (interiorCost > 0) {
+      reulstObject["facilities"] = {};
+      reulstObject["extentions"] = {};
+      reulstObject["extentions"]["Interior_Works"] = interiorCost;
+    } else {
+      for (const key in extentiosnCost) {
+        if (extentiosnCost.hasOwnProperty(key)) {
+          reulstObject["extentions"][extentiosnCost[key].select] =
+            extentiosnCost[key];
+        }
+      }
+    }
+
+    // cost counter
+    for (const key in reulstObject) {
+      if (Object.keys(reulstObject[key]).length > 0) {
+        let data = key;
+        // for facility
+        if (data == "facilities") {
+          for (const key in reulstObject[data]) {
+            for (const nKey in reulstObject[data]) {
+              totalCost += parseInt(reulstObject[data][nKey]);
+            }
+          }
+        } else if (data == "extentions") {
+          for (const nKey in reulstObject[data]) {
+            if (nKey == "Interior_Works") {
+              totalCost += parseInt(reulstObject[data][nKey]);
+            } else {
+              totalCost += parseInt(reulstObject[data][nKey].price);
+            }
+          }
+        } else {
+          // not for extentions or facility
+          for (const key in reulstObject[data]) {
+            totalCost += parseInt(reulstObject[data][key]);
           }
         }
       }
     }
 
-    // facilitiesCost
-    for (const key in facilitiesCost) {
-      if (facilitiesCost.hasOwnProperty(key)) {
-        totalCost += facilitiesCost[key] ? parseInt(facilitiesCost[key]) : 0;
+    // show result
+    let showPrices = false;
+    if (totalCost > 0) {
+      const pricesBox = document.getElementById("pricesBox");
+      const controllerBox = document.getElementById("controllerBox");
+
+      if (!showPrices && Object.keys(totalError).length <= 0) {
+        controllerBox.classList.add("hidden");
+        pricesBox.classList.remove("hidden");
+
+        // set value
+        document.getElementById("totalPrices").innerHTML = `£${formatNumber(
+          totalCost
+        )}*`;
+
+        // set items
+        let itemsBox = document.getElementById("items");
+        for (const key in reulstObject) {
+          if (Object.keys(reulstObject[key]).length > 0) {
+            let data = key;
+            // for facility
+            if (data == "facilities") {
+              for (const key in reulstObject[data]) {
+                let html = `<li
+                class="flex items-center justify-between py-2 px-3 border-b border-gray-200 bg-gray-50"
+              >
+                <span class="text-[14px] font-normal text-blue-950"
+                  >${key}</span
+                >
+                <span class="text-[14px] font-normal text-blue-950"
+                  >£${formatNumber(reulstObject[data][key])}*</span
+                >
+              </li>`;
+                itemsBox.innerHTML += html;
+              }
+            } else if (data == "extentions") {
+              for (const nKey in reulstObject[data]) {
+                if (nKey == "Interior_Works") {
+                  let html = `<li
+                class="flex items-center justify-between py-2 px-3 border-b border-gray-200 bg-gray-50"
+              >
+                <span class="text-[14px] font-normal text-blue-950"
+                  >Interior Works</span
+                >
+                <span class="text-[14px] font-normal text-blue-950"
+                  >£${formatNumber(reulstObject[data][nKey])}*</span
+                >
+              </li>`;
+                  itemsBox.innerHTML += html;
+                } else {
+                  let html = `<li
+                class="flex items-center justify-between py-2 px-3 border-b border-gray-200 bg-gray-50"
+              >
+                <span class="text-[14px] font-normal text-blue-950"
+                  >
+                  ${
+                    nKey == "groundFloorSettings"
+                      ? "Ground Floor Extension"
+                      : ""
+                  }
+                  ${nKey == "twoStoreySettings" ? "Two-Storey Extension" : ""}
+                  ${nKey == "conversionSettings" ? "Loft Conversion" : ""}
+                  </span
+                >
+                <span class="text-[14px] font-normal text-blue-950"
+                  >£${formatNumber(reulstObject[data][nKey].price)}*</span
+                >
+              </li>`;
+                  itemsBox.innerHTML += html;
+                }
+              }
+            } else {
+              for (const key in reulstObject[data]) {
+                let html = `<li
+                class="flex items-center justify-between py-2 px-3 border-b border-gray-200 bg-gray-50"
+              >
+                <span class="text-[14px] font-normal text-blue-950"
+                  >${key}</span
+                >
+                <span class="text-[14px] font-normal text-blue-950"
+                  >£${formatNumber(reulstObject[data][key])}*</span
+                >
+              </li>`;
+                itemsBox.innerHTML += html;
+              }
+            }
+          }
+        }
+
+        // send mail
+        
+      } else {
+        pricesBox.classList.add("hidden");
+        controllerBox.classList.remove("hidden");
       }
     }
+  }
 
-    console.log(totalCost);
+  // number formater
+  function formatNumber(num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 });
